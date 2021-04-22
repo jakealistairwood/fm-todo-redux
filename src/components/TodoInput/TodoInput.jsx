75fd8869d078a9f1d716c6045c0316a9db537d17
Jake Wood <jakealistairwood@gmail.com>
2021-04-22 22:14:23 +0100

    user can't submit an empty todo and added enter keypress functionality

--- src/components/TodoInput/TodoInput.jsx
+++ src/components/TodoInput/TodoInput.jsx
@@ -8,19 +8,34 @@ const TodoInput = () => {
     const dispatch = useDispatch();
 
     const addTodo = () => {
-        console.log(`Adding ${todo}`);
-        dispatch(saveTodo({
-            item: todo,
-            done: false,
-            id: Date.now()
-        }))
-        setTodo('');
+        if(todo.length > 0) {
+            console.log(`Adding ${todo}`);
+            dispatch(saveTodo({
+                item: todo,
+                done: false,
+                id: Date.now()
+            }))
+            setTodo('');
+        } else {
+            return;
+        }
+    }
+
+    const handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            addTodo();
+        }
     }
 
     return (
         <div className={`${styles.todoInput} ${styles.container}`}>
             <button onClick={addTodo}>+</button>
-            <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
+            <input 
+                type="text" 
+                value={todo} 
+                onChange={e => setTodo(e.target.value)} 
+                onKeyDown={handleKeyPress}
+                placeholder="Create a new todo..." />
         </div>
     )
 }
