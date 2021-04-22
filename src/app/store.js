9f5333c91ddb84719fcfc15d74235f18382de646
Jake Wood <jakealistairwood@gmail.com>
2021-04-22 15:13:46 +0100

    set up reducer for todoList

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from '../features/todoSlice';
+
+// The store is used to keep track of the state 
 
 export const store = configureStore({
   reducer: {
-  },
+    todos: todoReducer
+  }
 });
--- src/components/TodoInput/TodoInput.jsx
+++ src/components/TodoInput/TodoInput.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import styles from './TodoInput.module.scss';
+import { useDispatch } from 'react-redux';
+import { saveTodo } from '../../features/todoSlice';
 
 const TodoInput = () => {
     const [ todo, setTodo ] = useState('');
+    const dispatch = useDispatch();
 
     const addTodo = () => {
-
+        console.log(`Adding ${todo}`);
+        dispatch(saveTodo({
+            item: todo,
+            done: false,
+            id: Date.now()
+        }))
     }
 
     return (
-        <div className="todoInput">
+        <div className={`${styles.todoInput} ${styles.container}`}>
+            <button onClick={addTodo}>+</button>
             <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
-            <button onClick={addTodo}>Add</button>
         </div>
     )
 }
