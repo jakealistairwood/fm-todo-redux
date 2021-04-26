904ca6361702d57c8502378fd10687415e830b30
Jake Wood <jakealistairwood@gmail.com>
2021-04-26 14:55:34 +0100

    saving changes

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,48 +1,3 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// // const [ todoList, setTodoList ] = useState([]);
-
-// const initialState = {
-//     todoList: [],
-// }
-
-// const todoSlice = createSlice({
-//     name: 'todos',
-//     initialState,
-//     reducers: {
-//         saveTodo: (state, action) => {
-//             state.todoList.push(action.payload)
-//         },
-//         setCheck: (state, action) => {
-//             state.todoList.map(item => {
-//                 if (action.payload === item.id) {
-//                     if (item.done === true) {
-//                         item.done = false
-//                     } else {
-//                         item.done = true
-//                     }
-//                 }
-//             })
-//         },
-//         clearTodos: (state, action) => {
-//             state.todoList = [];
-//         },
-//         // clearTodos: (state, action) => {
-//         //     state.todoList.filter(item => {
-//         //         if(item.done === true) {
-
-//         //         }
-//         //     })
-//         // }
-//     }
-// });
-
-// export const { saveTodo, setCheck, clearTodos } = todoSlice.actions;
-
-// export const selectTodoList = state => state.todos.todoList;
-
-// export default todoSlice.reducer;
-
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = [];
@@ -70,7 +25,7 @@ const todoSlice = createSlice({
         },
         deleteCompletedTodos: (state) => {
             return state.filter(todo => todo.completed === false);
-        }
+        },
     }
 })
 
@@ -78,7 +33,7 @@ export const {
     createTodo,
     toggleCompletedTodo,
     deleteTodo,
-    deleteCompletedTodos
+    deleteCompletedTodos,
 } = todoSlice.actions;
 
 export default todoSlice.reducer;
